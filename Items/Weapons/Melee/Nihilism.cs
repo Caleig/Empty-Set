dab3ce711b0d0c3bcf031c0e69a40c6fd06b84a1
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-07-04 14:14:23 +0800

    3：新增武器奈尔丽增，阴云雷切，删除烁金镰

---
+++ Items/Weapons/Melee/Nihilism.cs
@@ -0,0 +1,47 @@
+﻿using EmptySet.Items.Materials;
+using EmptySet.Projectiles.Melee;
+using EmptySet.Utils;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace EmptySet.Items.Weapons.Melee;
+
+/// <summary>
+/// 奈尔丽增
+/// </summary>
+public class Nihilism : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+
+        CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+    }
+    public override void SetDefaults()
+    {
+        Item.width = 70;
+        Item.height = 70;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.useTime = UseSpeedLevel.SlowSpeed;
+        Item.useAnimation = UseSpeedLevel.SlowSpeed;
+        Item.DamageType = DamageClass.Melee;
+        Item.damage = 130;
+        Item.knockBack = KnockBackLevel.BeLower;
+        Item.crit = 12 - 4;
+        Item.value = Item.sellPrice(0, 2, 3, 6);
+        Item.rare = ItemRarityID.White;
+        Item.UseSound = SoundID.Item71;
+        Item.autoReuse = true;
+        Item.useTurn = true;
+        Item.shoot = ModContent.ProjectileType<TalosSickleProj>();
+        Item.shootSpeed = 5;
+    }
+
+    public override void AddRecipes() => CreateRecipe()
+        .AddIngredient(ModContent.ItemType<EternityAshBar>(), 15)
+        .AddIngredient(1508, 2)
+        .AddTile(TileID.MythrilAnvil)
+        .Register();
+}
