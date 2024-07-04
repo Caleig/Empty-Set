13736deabff8d542400e593183d0205ba125d00d
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-07-04 14:07:36 +0800

    3：新增武器阴云雷切

---
+++ Projectiles/Weapons/Melee/whirlpoolProj.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EmptySet.Projectiles.Weapons.Melee;
+
+/// <summary>
+/// 雷涡弹幕
+/// </summary>
+public class whirlpoolProj : ModProjectile
+{
+
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 38;
+        Projectile.height = 38;//54
+        Projectile.friendly = true;
+        Projectile.penetrate = 4 + 1;
+        Projectile.timeLeft = (int)(12 * EmptySet.Frame);
+
+
+        Projectile.DamageType = DamageClass.Melee;
+        Projectile.ignoreWater = false;
+        Projectile.aiStyle = -1;
+    }
+
+
+    public override void AI()
+    {
+        Projectile.rotation += 0.2f * Projectile.direction;
+        Projectile.velocity *= 0.985f;
+        var dust1 = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Silver, 0, 0, 0, Color.Cyan);
+        dust1.noGravity = true;
+        dust1.noLight = false;
+    }
+}
