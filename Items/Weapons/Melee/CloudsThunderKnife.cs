61f9758d3822f20a1beeebca4e7a2ef5de23958a
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-07-06 16:23:58 +0800

    阴云雷切补全

---
+++ Items/Weapons/Melee/CloudsThunderKnife.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using EmptySet.Items.Materials;
+using EmptySet.Projectiles.Weapons.Melee;
+using EmptySet.Utils;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace EmptySet.Items.Weapons.Melee;
+
+/// <summary>
+/// 阴云雷切
+/// </summary>
+public class CloudsThunderKnife : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+
+        CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.rare = ItemRarityID.Green;
+        Item.value = Item.sellPrice(0, 2, 15, 0);
+
+        Item.width = 50;
+        Item.height = 50;
+
+        Item.damage = 21;
+        Item.crit = 10;
+
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.knockBack = 1;
+        Item.useAnimation = 32;
+        Item.useTime = 32;
+        Item.UseSound = SoundID.Item77; //镰刀
+
+        Item.DamageType = DamageClass.Melee;
+
+        Item.autoReuse = true;
+        Item.shoot = ModContent.ProjectileType<whirlpoolProj>();
+        Item.shootSpeed = 7f;
+    }
+
+    public override void AddRecipes() => CreateRecipe()
+        .AddIngredient(ModContent.ItemType<RaySoul>(), 4)
+        .AddIngredient(ItemID.RainCloud, 7)
+        .AddIngredient(ItemID.Cloud, 5)
+        .AddTile(TileID.Anvils)
+        .Register();
+
+}
