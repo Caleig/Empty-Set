64c4332759917785142f32c066911755f7dfed78
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-07-17 18:40:50 +0800

    弹幕修改

--- Projectiles/Flails/DarkCrystal.cs
+++ Projectiles/Flails/DarkCrystal.cs
@@ -16,17 +16,24 @@ internal class DarkCrystal : ModProjectile
     }
     public override void SetDefaults()
     {
-        Projectile.width = 18;
-        Projectile.height = 18;
+        Projectile.width = 20;
+        Projectile.height = 20;
         Projectile.aiStyle = -1;
         Projectile.friendly = true;
         Projectile.light = 0.5f;
         Projectile.timeLeft = 5 * EmptySet.Frame;
         Projectile.tileCollide = false;
+
+        Projectile.DamageType = DamageClass.Melee;
+        Projectile.ignoreWater = false;
+        Projectile.aiStyle = -1;
     }
     public override void AI()
     {
-        Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        Projectile.rotation += 0.5f * Projectile.direction;
+        var dust1 = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Silver, 0, 0, 0, Color.Purple);
+        dust1.noGravity = true;
+        dust1.noLight = false;
     }
     public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
     {
