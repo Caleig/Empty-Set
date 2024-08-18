cc0160d8d394cb5db2318ccc467636fe64978bfc
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-08-19 01:39:35 +0800

    新增饰品[利锐佩刀][蓝眼石]

---
+++ Items/Accessories/BlueEyE.cs
@@ -0,0 +1,52 @@
+﻿using EmptySet.Common.Effects.Common;
+using EmptySet.Common.Effects.Item;
+using EmptySet.Common.Systems;
+using EmptySet.Tiles;
+using EmptySet.Utils;
+using System.Linq;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace EmptySet.Items.Accessories;
+
+/// <summary>
+///蓝眼石
+/// </summary>
+public class BlueEyE : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+    }
+    public override void SetDefaults()
+    {
+        Item.width = 14;
+        Item.height = 14;
+        Item.value = Item.sellPrice(0, 0, 70, 0);
+        Item.rare = ItemRarityID.LightRed;
+        Item.accessory = true;
+    }
+    public override bool CanEquipAccessory(Player player, int slot, bool modded)
+    {
+        var hasIt = player.armor.Any(x => x.type == ModContent.ItemType<Graypendant>());
+        if (hasIt)
+        {
+            return false;
+        }
+        else
+        return true;
+    }
+    public override void AddRecipes() => CreateRecipe()
+        .AddIngredient(ModContent.ItemType<UlcerPupil>())
+        .AddIngredient(ItemID.PurificationPowder,20)
+        .AddTile(ModContent.TileType<PurifierTile>())
+        .Register();
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetCritChance(DamageClass.Generic) += 5f;
+        player.GetArmorPenetration(DamageClass.Generic) += 5;
+    }
+}
