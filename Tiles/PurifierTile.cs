ac32669dc6d4c598214a65e1446270dd5831d428
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-08-19 01:38:39 +0800

    新增制作站[净化仪]

---
+++ Tiles/PurifierTile.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using EmptySet.Common.Systems;
+using EmptySet.Items.Placeable;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace EmptySet.Tiles
+{
+    internal class PurifierTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            DustType = 4;
+            
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16 };
+            TileObjectData.addTile(Type);
+
+            LocalizedText name = CreateMapEntryName();
+            // name.SetDefault("崩溃祭坛");
+            AddMapEntry(new Color(200, 200, 200), name);
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<Purifier>();
+        }
+    }
+}
