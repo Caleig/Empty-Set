c891438e9bacec790c63fe1b46b908728b64718d
Caleig <1359939188@qq.com>
2024-08-19 16:26:12 +0800

    翻译修复

--- obj/Debug/net6.0/EmptySet.AssemblyInfo.cs
+++ obj/Debug/net6.0/EmptySet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmptySet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b34277807acbeec655ff346897887ebc17050d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8259da7568112dc43c49d9b1e9c09c77c4f361")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmptySet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmptySet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
