acc2b2e828393a60f584e803c58ec0a0dba07f9b
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-08-27 15:23:52 +0800

    新物品克苏鲁之牙

---
+++ Common/ItemDropWay/FangofCthulhuDrop.cs
@@ -0,0 +1,19 @@
+﻿using EmptySet.Extensions;
+using EmptySet.Items.Weapons.Throwing;
+using EmptySet.Utils;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EmptySet.Common.ItemDropWay;
+
+public class FangofCthulhuDrop : GlobalNPC
+{
+    private static readonly int[] DropNpcList =
+    {
+        NPCID.EyeofCthulhu //史莱姆王
+    };
+
+    public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot) => npcLoot.AddManualLoot(DropNpcList, npc,
+        ItemDrop.GetItemDropRule<FangofCthulhu>(100, 20, 40));
+}
