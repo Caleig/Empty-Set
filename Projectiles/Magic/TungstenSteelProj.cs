8b5aa7aa58b4adc622bec441b047ba1d5bcdddbe
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-08-29 17:40:53 +0800

    新弹幕

---
+++ Projectiles/Magic/TungstenSteelProj.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EmptySet.Projectiles.Magic;
+
+/// <summary>
+/// 钨钢球
+/// </summary>
+public class TungstenSteelProj : ModProjectile
+{
+    public override void SetDefaults()
+    {
+        Projectile.width = 12;
+        Projectile.height = 12;
+        Projectile.scale = 0.9f;
+        Projectile.penetrate = 1;
+        Projectile.friendly = true;
+        Projectile.DamageType = DamageClass.Melee;
+        Projectile.timeLeft = 6 * EmptySet.Frame;
+        Projectile.light = 0.35f;
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        for (int i = 0; i < 5; i++)
+            Dust.NewDust(Projectile.Center + Projectile.velocity, Projectile.width, Projectile.height, DustID.GemDiamond, Projectile.velocity.X, Projectile.velocity.Y, 255, default(Color), 1.2f);
+        SoundEngine.PlaySound(SoundID.Item20, Projectile.position);
+        {
+            Projectile projectile = Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), Projectile.position, Vector2.Zero,
+                ModContent.ProjectileType<TungstenSteelExplode>(), (int)Projectile.damage, 1f, Projectile.owner);
+            projectile.Center = Projectile.Center;
+        }
+    }
+
+    public override void AI()
+    {
+        Projectile.localAI[0]++;
+        Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        var dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.GemDiamond);
+        dust.scale = 1.5f;
+        dust.noGravity = true;
+    }
+
+    public override bool OnTileCollide(Vector2 oldVelocity)
+    {
+        SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        for (int i = 0; i < 5; i++)
+            Dust.NewDust(Projectile.Center + Projectile.velocity, Projectile.width, Projectile.height, DustID.GemDiamond, Projectile.velocity.X, Projectile.velocity.Y, 255, default(Color), 1.2f);
+        return base.OnTileCollide(oldVelocity);
+    }
+}
+public class TungstenSteelExplode : ModProjectile
+{
+    public override string Texture => "Terraria/Images/Projectile_0";
+    public override void SetDefaults()
+    {
+        Projectile.width = 20;
+        Projectile.height = 20;
+        Projectile.friendly = true;
+        Projectile.ignoreWater = false;
+        Projectile.tileCollide = false;
+        Projectile.penetrate = -1;
+        Projectile.DamageType = DamageClass.Melee;
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.localNPCHitCooldown = -1;
+        Projectile.light = 1f;
+        Projectile.timeLeft = 5;
+    }
+    public override void AI()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+
+            float num1 = Main.rand.Next(-20, 20);
+            float num2 = Main.rand.Next(-20, 20);
+            float num3 = (float)Math.Sqrt(num1 * num1 + num2 * num2);
+            num3 = Main.rand.Next(2, 4) / num3;
+            num1 *= num3;
+            num2 *= num3;
+            Dust dust = Dust.NewDustDirect(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.GemDiamond, Scale: Main.rand.NextFloat(1f, 2f));
+            dust.noGravity = true;
+            dust.position = Projectile.Center;
+            dust.position += new Vector2((float)Main.rand.Next(-5, 6), (float)Main.rand.Next(-5, 6));
+            dust.velocity.X = num1;
+            dust.velocity.Y = num2;
+        }
+    }
+}
