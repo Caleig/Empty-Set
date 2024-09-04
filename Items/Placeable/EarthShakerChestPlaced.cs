4d97cd017a30eee64059da33afca62b93dc10150
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-09-04 16:34:23 +0800

    给3个匣子弄了个可放置版

---
+++ Items/Placeable/EarthShakerChestPlaced.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using EmptySet.Items.Consumables;
+using EmptySet.Tiles;
+using EmptySet.Utils;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace EmptySet.Items.Placeable;
+
+/// <summary>
+/// 匣子
+/// </summary>
+public class EarthShakerChestPlaced : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 48;
+        Item.height = 48;
+        Item.maxStack = 999;
+        Item.rare = ItemRarityID.Purple;
+        Item.value = Item.sellPrice(0, 0, 0, 0);
+        Item.useTurn = true;
+        Item.useTime = UseSpeedLevel.FastSpeed - 3;
+        Item.useAnimation = UseSpeedLevel.FastSpeed - 3;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.consumable = true;
+        Item.createTile = ModContent.TileType<EarthShakerChestTile>();
+    }
+
+    public override void AddRecipes() => CreateRecipe()
+        .AddIngredient(ModContent.ItemType<EarthShakerChest>(),1)
+        .AddTile(TileID.HeavyWorkBench)
+        .Register();
+}
