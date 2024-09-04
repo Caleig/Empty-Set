ae5045e95c90d61ed90c972212bb1e128b7748f0
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-09-04 16:33:30 +0800

    给3个匣子弄了个可放置版

---
+++ Tiles/EarthShakerChestTile.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using EmptySet.Common.Systems;
+using EmptySet.Items.Placeable;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace EmptySet.Tiles
+{
+    internal class EarthShakerChestTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            DustType = 92;
+            
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
+            TileObjectData.addTile(Type);
+
+            LocalizedText name = CreateMapEntryName();
+            AddMapEntry(new Color(83, 128, 151), name);
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<EarthShakerChestPlaced>();
+        }
+    }
+}
---
+++ Tiles/LavaHunterChestTile.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using EmptySet.Common.Systems;
+using EmptySet.Items.Placeable;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace EmptySet.Tiles
+{
+    internal class LavaHunterChestTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            DustType = 14;
+            
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
+            TileObjectData.addTile(Type);
+
+            LocalizedText name = CreateMapEntryName();
+            AddMapEntry(new Color(60, 40, 60), name);
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<LavaHunterChestPlaced>();
+        }
+    }
+}
