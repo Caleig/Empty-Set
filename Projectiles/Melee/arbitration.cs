6f72ef091a2145418c7267e75a5b672f4c31e23b
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-09-13 18:29:02 +0800

    仲裁重置

---
+++ Projectiles/Melee/arbitration.cs
@@ -0,0 +1,59 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using EmptySet.Extensions;
+using System.Diagnostics.Metrics;
+using Microsoft.CodeAnalysis;
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+
+namespace EmptySet.Projectiles.Melee;
+
+/// <summary>
+/// Ó°Çòµ¯Ä»(ÓÒ²à)
+/// </summary>
+public class arbitration : ModProjectile
+{
+    Vector2[] vec = new Vector2[1];
+    public override void SetDefaults()
+    {
+        Projectile.width = 130;
+        Projectile.height = 130;
+        Projectile.aiStyle = -1;
+        Projectile.tileCollide = false;
+        Projectile.timeLeft = 22;
+        Projectile.scale = 1.5f;
+        Projectile.alpha = 0;
+    }
+    public override void AI()
+    {
+        Projectile.alpha += 10;
+        if (Main.time % 3 == 0)
+        {
+
+            vec[0] = Projectile.Center;
+        }
+        float num8 = Projectile.rotation + Main.rand.NextFloatDirection() * ((float)Math.PI / 2f) * 0.7f;
+        Vector2 vector2 = Projectile.Center + num8.ToRotationVector2() * 84f * Projectile.scale;
+        Vector2 vector3 = (num8 + Projectile.ai[0] * ((float)Math.PI / 2f)).ToRotationVector2();
+        if (Main.rand.NextFloat() * 2f < Projectile.Opacity)
+        {
+            Dust dust2 = Dust.NewDustPerfect(Projectile.Center + num8.ToRotationVector2() * (Main.rand.NextFloat() * 80f * Projectile.scale + 20f * Projectile.scale), 278, vector3 * 1f, 100, Color.Lerp(Color.Gold, Color.White, Main.rand.NextFloat() * 0.3f), 0.4f);
+            dust2.fadeIn = 0.4f + Main.rand.NextFloat() * 0.15f;
+            dust2.noGravity = true;
+        }
+        if (Main.rand.NextFloat() * 1.5f < Projectile.Opacity)
+        {
+            Dust.NewDustPerfect(vector2, 43, vector3 * 1f, 100, Color.DarkRed * Projectile.Opacity, 1.2f * Projectile.Opacity);
+        }
+        Player player = Main.player[Projectile.owner];
+        Projectile.Center = player.Center + new Vector2(player.width, player.height / 2);
+        Projectile.rotation = player.itemRotation + (MathHelper.PiOver4 / 2);
+        base.AI();
+    }
+}
---
+++ Projectiles/Melee/arbitrationProj2.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EmptySet.Projectiles.Melee;
+
+/// <summary>
+/// 钨钢剑弹幕
+/// </summary>
+
+public class arbitrationProj2 : ModProjectile
+{
+    int _HitTime = 0;
+    int _HitTime2 = 0;
+    private Texture2D tex;
+    private Vector2[] oldPosi = new Vector2[3];
+    private Vector2[] oldVec = new Vector2[3];
+    private float[] oldRot = new float[3];
+    private int frametime = 0;
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 7;
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        base.SetStaticDefaults();
+    }
+    public override void SetDefaults()
+    {
+        Projectile.width = 108; //已精确测量
+        Projectile.height = 108;
+        Projectile.friendly = true;
+        Projectile.penetrate = 2 + 1;
+        Projectile.DamageType = DamageClass.Melee;
+        Projectile.tileCollide = false;
+        Projectile.timeLeft = (12 * EmptySet.Frame);
+        Projectile.alpha = 50;
+        Projectile.Opacity = 0.6f;
+        tex = ModContent.Request<Texture2D>("EmptySet/Projectiles/Melee/TungstenSteelSwordProj").Value;
+        
+    }
+    public override void AI()
+    {
+        Projectile.rotation += 0.4f * Projectile.direction;
+        if (Projectile.timeLeft < (11.5f * EmptySet.Frame))
+        {
+            Projectile.velocity *= 0.9f;
+            Projectile.Opacity -= 0.02f;
+        }
+        else if(_HitTime2 > 0)
+            {
+                Projectile.velocity *= 0.9f;
+                Projectile.Opacity -= 0.05f;
+        }
+        if (Projectile.Opacity < 0)
+        {
+            Projectile.Kill();
+            _HitTime2 = 0;
+        }
+            
+    }
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        _HitTime2 = 1;
+    }
+    public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+    {
+        _HitTime++;
+        if (_HitTime >= 3)
+        {
+            modifiers.SetCrit();
+        }
+        base.ModifyHitNPC(target, ref modifiers);
+    }
+    public override Color? GetAlpha(Color lightColor)
+    {
+        return new Color(255, 255, 200, 0) * Projectile.Opacity;
+    }
+
+    public override bool PreDraw(ref Color lightColor)
+    {
+        Texture2D texture2D13 = TextureAssets.Projectile[Projectile.type].Value;
+        int num156 = TextureAssets.Projectile[Projectile.type].Value.Height / Main.projFrames[Projectile.type];
+        int y3 = num156 * Projectile.frame;
+        Rectangle rectangle = new Rectangle(0, y3, texture2D13.Width, num156);
+        Vector2 origin2 = rectangle.Size() / 2f;
+
+        Color color26 = lightColor;
+        color26 = Projectile.GetAlpha(color26);
+
+        for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[Projectile.type]; i++)
+        {
+            Color color27 = color26;
+            color27 *= (float)(ProjectileID.Sets.TrailCacheLength[Projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[Projectile.type];
+            Vector2 value4 = Projectile.oldPos[i];
+            float num165 = Projectile.oldRot[i];
+            Main.EntitySpriteDraw(texture2D13, value4 + Projectile.Size / 2f - Main.screenPosition + new Vector2(0, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, Projectile.scale, SpriteEffects.None, 0);
+        }
+
+        Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Projectile.GetAlpha(lightColor), Projectile.rotation, origin2, Projectile.scale, SpriteEffects.None, 0);
+        return false;
+    }
+}
