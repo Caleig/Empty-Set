108c8900fe6047ca3b1948843b8a75b056d034c6
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-09-21 18:09:09 +0800

    调整·

--- Items/Weapons/Melee/Nihilism.cs
+++ Items/Weapons/Melee/Nihilism.cs
@@ -1,10 +1,14 @@
-﻿using EmptySet.Items.Materials;
+﻿using EmptySet.Common.Players;
+using EmptySet.Common.Systems;
+using EmptySet.Items.Materials;
 using EmptySet.Projectiles.Melee;
 using EmptySet.Utils;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.GameContent.Creative;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace EmptySet.Items.Weapons.Melee;
@@ -44,4 +48,20 @@ public class Nihilism : ModItem
         .AddIngredient(1508, 2)
         .AddTile(TileID.MythrilAnvil)
         .Register();
+    public override bool CanUseItem(Player player)
+    {
+
+        return true;
+
+    }
+
+    public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
+    {
+        if (Main.rand.NextBool(1))
+        {
+            Dust.NewDustDirect(new Vector2(0, -100).RotatedBy(player.itemRotation + MathHelper.ToRadians(30 * player.direction)) + player.Center, 2, 2, DustID.Silver).noGravity = true;
+            Dust.NewDustDirect(new Vector2(0, -100).RotatedBy(player.itemRotation + MathHelper.ToRadians(30 * player.direction)) + player.Center, 2, 2, DustID.SilverCoin).noGravity = true;
+        }
+        base.UseItemHitbox(player, ref hitbox, ref noHitbox);
+    }
 }
