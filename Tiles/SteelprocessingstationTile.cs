6e8ca9649d7151a57b157c0b6ed8cb58c9f91029
Nephren0Ruq0Insania <a267036861@foxmail.com>
2024-09-29 15:27:12 +0800

    Add files via upload

---
+++ Tiles/SteelprocessingstationTile.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using EmptySet.Common.Systems;
+using EmptySet.Items.Placeable;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace EmptySet.Tiles
+{
+    internal class SteelprocessingstationTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            DustType = 6;
+            AdjTiles = new int[] { 17,77,16 };
+            
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16 };
+            TileObjectData.addTile(Type);
+
+            LocalizedText name = CreateMapEntryName();
+            AddMapEntry(new Color(141, 137, 143), name);
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<Steelprocessingstation>();
+        }
+    }
+}
